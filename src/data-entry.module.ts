f8fad9e537b9fb09fdc14504df7c8228c431133b
Abhishek Sharma <65721166+devabhi6848@users.noreply.github.com>
2026-09-15 14:40:58 +0530

    feat: register entity matching service

--- src/data-entry.module.ts
+++ src/data-entry.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DataEntryController } from './data-entry.controller';
 import { DataEntryService } from './data-entry.service';
+import { EntityMatchingService } from './entity-matching.service';
 import { MappingService } from './mapping.service';
 
 @Module({
   controllers: [DataEntryController],
-  providers: [DataEntryService, MappingService],
-  exports: [DataEntryService, MappingService],
+  providers: [DataEntryService, MappingService, EntityMatchingService],
+  exports: [DataEntryService, MappingService, EntityMatchingService],
 })
 export class DataEntryModule {}
