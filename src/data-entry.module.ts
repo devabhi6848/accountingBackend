c7f5a9f895b6f9213821a000cf00a0c107a7e5b9
Abhishek Sharma <65721166+devabhi6848@users.noreply.github.com>
2026-09-15 14:40:01 +0530

    Register mapping service

--- src/data-entry.module.ts
+++ src/data-entry.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DataEntryController } from './data-entry.controller';
 import { DataEntryService } from './data-entry.service';
+import { MappingService } from './mapping.service';
 
 @Module({
   controllers: [DataEntryController],
-  providers: [DataEntryService],
-  exports: [DataEntryService],
+  providers: [DataEntryService, MappingService],
+  exports: [DataEntryService, MappingService],
 })
 export class DataEntryModule {}
