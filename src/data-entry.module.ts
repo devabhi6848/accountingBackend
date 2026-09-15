af6fe74e6510ce7c0b1975fef63f3c111f2f826f
Abhishek Sharma <65721166+devabhi6848@users.noreply.github.com>
2026-09-15 14:31:22 +0530

    feat: register data entry module

---
+++ src/data-entry.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DataEntryController } from './data-entry.controller';
+import { DataEntryService } from './data-entry.service';
+
+@Module({
+  controllers: [DataEntryController],
+  providers: [DataEntryService],
+  exports: [DataEntryService],
+})
+export class DataEntryModule {}
