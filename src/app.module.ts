dbfc4e5ca485ec9d7e645ddbd274f483760c242d
Abhishek Sharma <65721166+devabhi6848@users.noreply.github.com>
2026-09-15 14:32:03 +0530

    feat: connect data entry module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DataEntryModule } from './data-entry.module';
 
 @Module({
   imports: [
@@ -7,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       cache: true,
     }),
+    DataEntryModule,
   ],
 })
 export class AppModule {}
