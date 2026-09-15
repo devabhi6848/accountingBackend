5babca4fd24d2386e47d4f019acb37fee7dea043
Abhishek Sharma <65721166+devabhi6848@users.noreply.github.com>
2026-09-15 14:45:47 +0530

    feat: register GST validation service

--- src/data-entry.module.ts
+++ src/data-entry.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { DataEntryController } from './data-entry.controller';
 import { DataEntryService } from './data-entry.service';
 import { EntityMatchingService } from './entity-matching.service';
+import { GstService } from './gst.service';
 import { MappingService } from './mapping.service';
 
 @Module({
   controllers: [DataEntryController],
-  providers: [DataEntryService, MappingService, EntityMatchingService],
-  exports: [DataEntryService, MappingService, EntityMatchingService],
+  providers: [DataEntryService, MappingService, EntityMatchingService, GstService],
+  exports: [DataEntryService, MappingService, EntityMatchingService, GstService],
 })
 export class DataEntryModule {}
