53ac46ab02c716cc01acd7cb610bbd7186bd6566
Abhishek Sharma <65721166+devabhi6848@users.noreply.github.com>
2026-09-15 14:46:49 +0530

    feat: register duplicate detection service

--- src/data-entry.module.ts
+++ src/data-entry.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DataEntryController } from './data-entry.controller';
 import { DataEntryService } from './data-entry.service';
+import { DuplicateService } from './duplicate.service';
 import { EntityMatchingService } from './entity-matching.service';
 import { GstService } from './gst.service';
 import { MappingService } from './mapping.service';
 
 @Module({
   controllers: [DataEntryController],
-  providers: [DataEntryService, MappingService, EntityMatchingService, GstService],
-  exports: [DataEntryService, MappingService, EntityMatchingService, GstService],
+  providers: [DataEntryService, MappingService, EntityMatchingService, GstService, DuplicateService],
+  exports: [DataEntryService, MappingService, EntityMatchingService, GstService, DuplicateService],
 })
 export class DataEntryModule {}
